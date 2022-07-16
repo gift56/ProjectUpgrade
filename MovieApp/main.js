8ecc338539c94fb6b7af45a6391baa8cd9a817dd
gift56 <efeasiughu@gmail.com>
2022-07-16 15:19:30 +0100

    fetch the items

--- MovieApp/main.js
+++ MovieApp/main.js
@@ -24,5 +24,17 @@ const showMovies = (movies) => {
 
         const movieContainer = document.createElement('div')
         movieContainer.classList.add('movie')
+
+        movieContainer.innerHTML = `
+        <img scr="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+           <h3>Overview</h3>
+           ${overview}
+        </div>
+        `
     })
 }
