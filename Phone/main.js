691315c65ce87667ecb3c61cae6fdc34154cd738
gift56 <efeasiughu@gmail.com>
2022-07-16 11:43:24 +0100

    fixed last img

--- Phone/main.js
+++ Phone/main.js
@@ -2,12 +2,12 @@ const slides = document.querySelector(".slideImg");
 const Next = document.getElementById("Next");
 const Prev = document.getElementById("Prev");
 
-Prev.addEventListener('click', () => {
-    slides.style.left = "17px";
-})
+// Prev.addEventListener('click', () => {
+//     slides.style.left = "17px";
+// })
 Next.addEventListener('click', () => {
     slides.style.left = "-232px";
 })
-More.addEventListener('click', () => {
-    slides.style.left = "-488px";
-})
+// More.addEventListener('click', () => {
+//     slides.style.left = "-488px";
+// })
