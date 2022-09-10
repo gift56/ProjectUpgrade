55ab7fb2afe7bd74622cf2053d54a6370491157d
gift56 <efeasiughu@gmail.com>
2022-09-10 21:38:33 +0100

    writing js

---
+++ Range/main.js
@@ -0,0 +1,7 @@
+// const rangeSlider = (value) => {
+//   rangeValue.innerHTML = value;
+// };
+
+// rangeSlideChanger.addEventListener("mousemove", (e) => {
+//   rangeValue.innerHTML = e.value;
+// });
